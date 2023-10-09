d61d2991eac88764f7c09f93023af1bb11e535ba
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-10-09 17:05:05 +0200

    lesson 1 and 2 completed

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ const songs = [
 console.log(songs.indexOf('Circus'));
 songs.splice(3,1,);
 console.log(songs);
-*/
+
 
 
 //05
@@ -142,3 +142,159 @@ songs.forEach(function (song, index) {
     
     
 });
+
+
+//06
+const songs = [
+    'Toxic',
+    '...Baby one more time',
+    'Everytime',
+    'Circus',
+    'Oops! I did it again',
+    'Lucky',
+    "I'm not a girl, not yet a woman",
+];
+for (let i = 0; i < songs.length; i++){
+    const str = songs[i];
+    const reversedString = str.split("").reduce((acc, char) => char + acc, "");
+    console.log(reversedString);
+}
+
+
+
+
+//07
+const songs = [
+    'Toxic',
+    '...Baby one more time',
+    'Everytime',
+    'Circus',
+    'Oops! I did it again',
+    'Lucky',
+    "I'm not a girl, not yet a woman",
+];
+let i = 0;
+while (i < songs.length){
+    if(songs[i] === 'Lucky') {
+        console.log("Stop! Here's Lucky!");
+        break;
+    }
+    else {
+        console.log(songs[i]);
+        i++;
+    }
+}
+
+
+
+//08
+const songs = [
+    'Toxic',
+    '...Baby one more time',
+    'Everytime',
+    'Circus',
+    'Oops! I did it again',
+    'Lucky',
+    "I'm not a girl, not yet a woman",
+];
+const map1 = songs.map((song) => song.toUpperCase());
+console.log(map1);
+
+
+
+
+//09
+const person = {
+    name: 'Britney Jean Spears',
+    birthdate: '2 December, 1981',
+    from: 'McComb, Mississippi, USA',
+    length: 163,
+    children: ['Sean Federline', 'Jayden James Federline'],
+    description:
+      'Britney Spears has been one of the most successful — and sometimes controversial — solo acts in popular music. Six of her first seven albums reached No. 1 on the Billboard 200.',
+  };
+  console.log('Her name is '+person.name+' and she was born '+person.birthdate +' .');
+  
+
+
+  //10
+  const person = {
+    name: 'Britney Jean Spears',
+    birthdate: '2 December, 1981',
+    from: 'McComb, Mississippi, USA',
+    length: 163,
+    children: ['Sean Federline', 'Jayden James Federline'],
+    description:
+      'Britney Spears has been one of the most successful — and sometimes controversial — solo acts in popular music. Six of her first seven albums reached No. 1 on the Billboard 200.',
+  };
+  
+  person.children.forEach((children) => {
+    console.log(children);
+  })
+  
+
+
+  //11
+  const albums = [
+    { title: '...Baby One More Time', year: 1999, label: 'Jive' },
+    { title: 'Oops!... I Did It Again', year: 2000, label: 'Jive' },
+    { title: 'Britney', year: 2001, label: 'Jive' },
+    { title: 'In the zone', year: 2003, label: 'Jive, Zomba' },
+    { title: 'Blackout', year: 2007, label: 'Jive, Zomba' },
+    { title: 'Circus', year: 2008, label: 'Jive, Zomba' },
+    { title: 'Femme Fatale', year: 2011, label: 'Jive' },
+    { title: 'Britney Jean', year: 2013, label: 'RCA' },
+    { title: 'Glory', year: 2016, label: 'RCA' },
+  ];
+  albums.forEach((element) => {
+    console.log(element.title+','+element.year);
+  })
+  
+
+
+
+  //12
+  const albums = [
+    { title: '...Baby One More Time', year: 1999, label: 'Jive' },
+    { title: 'Oops!... I Did It Again', year: 2000, label: 'Jive' },
+    { title: 'Britney', year: 2001, label: 'Jive' },
+    { title: 'In the zone', year: 2003, label: 'Jive, Zomba' },
+    { title: 'Blackout', year: 2007, label: 'Jive, Zomba' },
+    { title: 'Circus', year: 2008, label: 'Jive, Zomba' },
+    { title: 'Femme Fatale', year: 2011, label: 'Jive' },
+    { title: 'Britney Jean', year: 2013, label: 'RCA' },
+    { title: 'Glory', year: 2016, label: 'RCA' },
+  ];
+
+  function getAlbumsFrom(array, year){
+    const result = array.filter((array) => array.year >= year);
+    return result;
+  }
+  console.log(getAlbumsFrom(albums, 2013));
+*/
+
+
+//13
+const albums = [
+    { title: '...Baby One More Time', year: 1999, label: 'Jive' },
+    { title: 'Oops!... I Did It Again', year: 2000, label: 'Jive' },
+    { title: 'Britney', year: 2001, label: 'Jive' },
+    { title: 'In the zone', year: 2003, label: 'Jive, Zomba' },
+    { title: 'Blackout', year: 2007, label: 'Jive, Zomba' },
+    { title: 'Circus', year: 2008, label: 'Jive, Zomba' },
+    { title: 'Femme Fatale', year: 2011, label: 'Jive' },
+    { title: 'Britney Jean', year: 2013, label: 'RCA' },
+    { title: 'Glory', year: 2016, label: 'RCA' },
+  ];
+  
+
+
+const labelCount = albums.reduce((accumulator, album) => {
+    const labels = album.label.split(', '); // använd split för att skapa en array med bara label value
+    labels.forEach((label) => {
+      accumulator[label] = (accumulator[label] || 0) + 1; // öka med ett för varje label
+    });
+    return accumulator;//returna antal gånger vi ökat värdet
+  }, {});
+  
+  console.log(labelCount);
